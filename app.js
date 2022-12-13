6e5c3a6480ae398cc481b6fa0918e3d3cb869100
balaji511 <balaji16f2@gmail.com>
2022-12-13 14:58:10 +0530

    evaluated loop

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 (() => {})();
 
 console.log(eval("665/5645"));
+console.log(eval("665/5645"));
+console.log(eval("665/5645"));
+console.log(eval("665/5645"));
+console.log(eval("665/5645"));
+console.log(eval("665/5645"));
+console.log(eval("665/5645"));
